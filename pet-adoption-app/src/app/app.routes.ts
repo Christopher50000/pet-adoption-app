f7300cfc83bf6081d1330b6e577126f7eac99cdb
Christopher Tupa <christoper.tupa61@gmail.com>
2025-08-03 14:38:10 -0500

    Project set up for file struc and components

--- pet-adoption-app/src/app/app.routes.ts
+++ pet-adoption-app/src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { Home } from './features/pages/home/home';
+import { AddPet } from './features/pages/add-pet/add-pet';
+import {ListPets} from './features/pages/list-pets/list-pets';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path: 'Home-Page',component: Home},
+  {path: 'Add-Pet',component: AddPet},
+  {path: 'List-Pets',component: ListPets}
+];
