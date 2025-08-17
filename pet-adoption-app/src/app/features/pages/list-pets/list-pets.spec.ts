ba9cba00f3d4696be26b27533bae7090b31ced46
Christopher Tupa <christoper.tupa61@gmail.com>
2025-08-17 17:14:32 -0500

    Added Unit test for pet list component

--- pet-adoption-app/src/app/features/pages/list-pets/list-pets.spec.ts
+++ pet-adoption-app/src/app/features/pages/list-pets/list-pets.spec.ts
@@ -1,23 +1,45 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ListPets } from './list-pets';
+import { PetService} from '../../services/pet-service';
+import {provideHttpClient} from '@angular/common/http';
+import {of} from 'rxjs';
+import {Pet} from '../../models/pet.model';
 
 describe('ListPets', () => {
   let component: ListPets;
-  let fixture: ComponentFixture<ListPets>;
+  let petServiceSpy: jasmine.SpyObj<PetService>;
 
   beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [ListPets]
-    })
-    .compileComponents();
+     petServiceSpy = jasmine.createSpyObj('PetService', ['getPets']);
 
-    fixture = TestBed.createComponent(ListPets);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+    TestBed.configureTestingModule({
+      providers: [
+        ListPets,
+        { provide: PetService, useValue: petServiceSpy },
+        provideHttpClient() // <-- THIS PROVIDES HttpClient FOR TEST
+      ]
+    });
+
+    petServiceSpy = TestBed.inject(PetService) as jasmine.SpyObj<PetService>
+    component = TestBed.inject(ListPets);
+
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('should load pets', () => {
+    // Arrange: mock data
+    const mockPets: Pet[] = [{ id: 1, name: 'Fido' }];
+    petServiceSpy.getPets.and.returnValue(of(mockPets));
+
+    // Act: initialize component (calls ngOnInit)
+    component.ngOnInit();
+
+    // Assert
+    expect(component.pets).toEqual(mockPets);
+    expect(component.loading).toBe(false);
+    expect(petServiceSpy.getPets).toHaveBeenCalled();
+    expect(petServiceSpy.getPets).toHaveBeenCalledTimes(1);
+
+
   });
 });
